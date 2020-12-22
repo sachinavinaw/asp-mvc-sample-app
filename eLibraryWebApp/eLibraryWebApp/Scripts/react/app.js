712fb15485cf99cde846dda8bcfd339a6a31557b
sachinavinaw <sachinavinaw@gmail.com>
2020-12-22 21:25:44 +0900

    removed unnecssary files

---
+++ eLibraryWebApp/eLibraryWebApp/Scripts/react/app.js
@@ -0,0 +1,2 @@
+﻿const x = "This is variable X";
+const y = "This is variable Y";
---
+++ eLibraryWebApp/eLibraryWebApp/Scripts/react/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require('path');
+module.exports = {
+    context: __dirname,
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
